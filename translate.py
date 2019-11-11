551c4061401f59ddf883151274c803791a6b3199
chenziku <chenzikun515@gmail.com>
2019-11-11 13:12:55 -0500

    Update translate.py

--- translate.py
+++ translate.py
@@ -5,22 +5,20 @@
 
 from __future__ import print_function
 import pickle
-from state import Board, Game
-from mcts_pure import MCTSPlayer as MCTS_Pure
-from mcts_alphaZero import MCTSPlayer
-from policy_value_net_numpy import PolicyValueNetNumpy
+from state import Vocab, Translate
+# from mcts_pure import MCTSPlayer as MCTS_Pure
+from mcts_alphazero import MCTSTranslator
+# from policy_value_net_numpy import PolicyValueNetNumpy
 # from policy_value_net import PolicyValueNet  # Theano and Lasagne
-# from policy_value_net_pytorch import PolicyValueNet  # Pytorch
+from policy_value_net_pytorch import PolicyValueNet  # Pytorch
 # from policy_value_net_tensorflow import PolicyValueNet # Tensorflow
 # from policy_value_net_keras import PolicyValueNet  # Keras
 
 def run():
-    n = 5
     model_file = 'best_policy_8_8_5.model'
     try:
-        state = State(width=width, height=height, n_in_row=n)
+        translate = Translate()
 
-        # ############### human VS AI ###################
         # load the trained policy_value_net in either Theano/Lasagne, PyTorch or TensorFlow
 
         # best_policy = PolicyValueNet(width, height, model_file = model_file)
@@ -32,20 +30,14 @@ def run():
         except:
             policy_param = pickle.load(open(model_file, 'rb'),
                                        encoding='bytes')  # To support python3
-        best_policy = PolicyValueNetNumpy(width, height, policy_param)
+        best_policy = PolicyValueNet(vocab_size, policy_param)
         
-        mcts_player = MCTSPlayer(best_policy.policy_value_fn,
+        mcts_translator = MCTSTranslator(best_policy.policy_value_fn,
                                  c_puct=5,
                                  n_playout=400)  # set larger n_playout for better performance
 
-        # uncomment the following line to play with pure MCTS (it's much weaker even with a larger n_playout)
-        # mcts_player = MCTS_Pure(c_puct=5, n_playout=1000)
-
-        # human player, input your move in the format: 2,3
-        human = Human()
-
         # set start_player=0 for human first
-        game.start_play(human, mcts_player, start_player=1, is_shown=1)
+        translate.start_translate(mcts_translator, is_shown=1)
     except KeyboardInterrupt:
         print('\n\rquit')
 
