added gradient stepfunctionality as well as fixed cython bugs

MultiProcess/dataBuffer.py
@@ -5,75 +5,75 @@ import numpy as np
 import globalsFile
 
 class DataBuffer:
-	def __init__(self):
-		self.src_tensors = []
-		self.dec_tensors = []
-		self.bleus = []
-		self.dists = [] #is pi in alpha go zero
-		self.actions = [] #is actions that each prob in dists corresponds to
+    def __init__(self):
+        self.src_tensors = []
+        self.dec_tensors = []
+        self.bleus = []
+        self.dists = [] #is pi in alpha go zero
+        self.actions = [] #is actions that each prob in dists corresponds to
 
-	def set_iterator(self,batch_size,shuffle=True):
-		self.num_iters = 0 #number of examples we've iterated over
-		self.batch_size = batch_size
-		self.examples_order = np.arange(self.len())
-		if shuffle:
-			np.random.shuffle(self.examples_order)
-
-
-	def len(self):
-		return len(self.src_tensors)
-
-	
-	def add_examples(self,src_,data):
-		bleu,output_states,mcts_probs,actions = data
-		
-		#print('len output_state: {}, type: {}'.format(len(output_states[0]),type(output_states[0])))
-		for i in range(len(output_states)):
-			self.src_tensors.append(src_.clone())
-			self.actions.append(torch.tensor(actions[i]))
-			self.dec_tensors.append(torch.tensor(output_states[i]))
-			self.dists.append(torch.tensor(mcts_probs[i]))
-			self.bleus.append(bleu)
-
-	def next(self):
-		#draw batch of size batch_size
-		inds = np.arange(self.num_iters,min(self.num_iters+self.batch_size,self.len()))
-		src_list = [self.src_tensors[i] for i in inds]
-		max_src_len = max([len(x) for x in src_list])
-
-		#now pad
-		src_mat = torch.ones((max_src_len,len(inds)))*globalsFile.BLANK_WORD_ID
-		for col,ind in enumerate(inds):
-			src_mat[:len(src_list[ind]),col] = src_list[ind]
-
-		dec_list = [self.dec_tensors[i] for i in inds]
-		max_dec_len = max([len(x) for x in dec_list])
-
-		#now pad
-		dec_mat = torch.ones((max_dec_len,len(inds)))*globalsFile.BLANK_WORD_ID
-		for col,ind in enumerate(inds):
-			dec_mat[:len(dec_list[ind]),col] = dec_list[ind]
-
-		bleus = torch.tensor([self.bleus[i] for i in inds])
-
-		#make batch dim=1 (so columns)
-		actions = torch.cat([self.actions[i].view(-1,1) for i in inds],dim=1)
-		probs = torch.cat([self.dists[i].view(-1,1) for i in inds],dim=1)
-
-		if self.num_iters+self.batch_size >= self.len():
-			return None,None,None,None,None #reached end of dataset
-		else:
-			self.num_iters = self.num_iters + self.batch_size
-		
-		#convert to appropriate types
-		src_mat = src_mat.long()
-		dec_mat = dec_mat.long()
-		actions = actions.long()
-		probs = probs.double()
-		bleus = bleus.double()
+    def set_iterator(self,batch_size,shuffle=True):
+        self.num_iters = 0 #number of examples we've iterated over
+        self.batch_size = batch_size
+        self.examples_order = np.arange(self.len())
+        if shuffle:
+            np.random.shuffle(self.examples_order)
 
 
-		return src_mat,dec_mat,actions,probs,bleus
+    def len(self):
+        return len(self.src_tensors)
+
+
+    def add_examples(self,src_,data):
+        bleu,output_states,mcts_probs,actions = data
+
+        #print('len output_state: {}, type: {}'.format(len(output_states[0]),type(output_states[0])))
+        for i in range(len(output_states)):
+            self.src_tensors.append(src_.clone())
+            self.actions.append(torch.tensor(actions[i]))
+            self.dec_tensors.append(torch.tensor(output_states[i]))
+            self.dists.append(torch.tensor(mcts_probs[i]))
+            self.bleus.append(bleu)
+
+    def next(self):
+        #draw batch of size batch_size
+        inds = np.arange(self.num_iters,min(self.num_iters+self.batch_size,self.len()))
+        src_list = [self.src_tensors[i] for i in inds]
+        max_src_len = max([len(x) for x in src_list])
+
+        #now pad
+        src_mat = torch.ones((max_src_len,len(inds)))*globalsFile.BLANK_WORD_ID
+        for col,ind in enumerate(inds):
+            src_mat[:len(src_list[ind]),col] = src_list[ind]
+
+        dec_list = [self.dec_tensors[i] for i in inds]
+        max_dec_len = max([len(x) for x in dec_list])
+
+        #now pad
+        dec_mat = torch.ones((max_dec_len,len(inds)))*globalsFile.BLANK_WORD_ID
+        for col,ind in enumerate(inds):
+            dec_mat[:len(dec_list[ind]),col] = dec_list[ind]
+
+        bleus = torch.tensor([self.bleus[i] for i in inds])
+
+        #make batch dim=1 (so columns)
+        actions = torch.cat([self.actions[i].view(-1,1) for i in inds],dim=1)
+        probs = torch.cat([self.dists[i].view(-1,1) for i in inds],dim=1)
+
+        if self.num_iters+self.batch_size >= self.len():
+            return None,None,None,None,None #reached end of dataset
+        else:
+            self.num_iters = self.num_iters + self.batch_size
+
+        #convert to appropriate types
+        src_mat = src_mat.long()
+        dec_mat = dec_mat.long()
+        actions = actions.long()
+        probs = probs.double()
+        bleus = bleus.double()
+
+
+        return src_mat,dec_mat,actions,probs,bleus
 
 
 

MultiProcess/policy_net.py
@@ -114,7 +114,8 @@ class TransformerModel(nn.Module):
 class MainParams:
     def __init__(self, dropout, src_vocab_size, tgt_vocab_size, 
                 batch_size,l2_const,c_puct,num_sims,temperature,
-                tgt_vocab_itos,num_children,is_training):
+                tgt_vocab_itos,num_children,is_training,num_grad_steps_per_epoch=4,
+                  adamlr=1e-4):
         use_gpu = torch.cuda.is_available()
         self.device = torch.device("cuda:0" if use_gpu else "cpu")
         print(self.device)
@@ -130,11 +131,14 @@ class MainParams:
         self.num_children = num_children
         self.temperature = temperature 
         self.is_training = is_training
+        self.num_grad_steps_per_epoch = num_grad_steps_per_epoch
+        self.adamlr = adamlr
+        
 
 class PolicyValueNet():
     """policy-value network"""
 
-    def __init__(self, main_params, path_to_policy=None, path_to_value=None):
+    def __init__(self, main_params, path_to_policy=None, path_to_value=None,adamlr=1e-4):
         self.use_gpu = True if main_params.device == torch.device('cuda:0') else False
         self.l2_const = main_params.l2_const  # coef of l2 penalty
         self.device = main_params.device
@@ -170,9 +174,9 @@ class PolicyValueNet():
             
         # optimizer
         self.policy_optimizer = optim.Adam(
-            self.policy_net.parameters(), weight_decay=self.l2_const)
+            self.policy_net.parameters(), weight_decay=self.l2_const, lr=adamlr)
         self.value_optimizer = optim.Adam(
-            self.value_net.parameters(), weight_decay=self.l2_const)
+            self.value_net.parameters(), weight_decay=self.l2_const, lr=adamlr)
 
 
     def forward(self, src_tensor, dec_input, processes, sentence_lens,req_grad=False):
@@ -205,9 +209,9 @@ class PolicyValueNet():
         src_key_padding_mask = (src_tensor == 1).transpose(0, 1)
         dec_key_padding_mask = (dec_input==1).transpose(0,1)
         
-        if not self.encoder_output is None:
-            print('SHAPES: {}, {},{}'.format(src_key_padding_mask.shape,encoder_output_sliced.shape,src_tensor.shape))
-            print('PRocesses: ',processes)
+        #if not self.encoder_output is None:
+        #    print('SHAPES: {}, {},{}'.format(src_key_padding_mask.shape,encoder_output_sliced.shape,src_tensor.shape))
+        #    print('PRocesses: ',processes)
         #print(dec_key_padding_mask)
         #print(dec_key_padding_mask.shape)
         #print('PRINTED HERE')
@@ -219,8 +223,8 @@ class PolicyValueNet():
                                              memory_key_padding_mask=src_key_padding_mask,
                                              memory=encoder_output_sliced)
             
-            log_act_probs = F.log_softmax(policy_output[sentence_lens-1, batch_indices, :], dim=1)
-            #print('SHAPE log_act_probs: ',log_act_probs.shape)
+            act_probs = F.softmax(policy_output[sentence_lens-1, batch_indices, :], dim=1)
+            #the shape is (batch_size, vocab size)
 
             #commenting out shared decoder_embedding for now since were 
             #trained with different ones which will throw off the algorithm initially
@@ -241,7 +245,7 @@ class PolicyValueNet():
                 self.encoder_output = encoder_output_sliced.clone()
 
             value_output = torch.sigmoid(value_output[sentence_lens-1, batch_indices, 0])
-            return log_act_probs, value_output
+            return act_probs, value_output
 
     
     '''
@@ -250,15 +254,18 @@ class PolicyValueNet():
     '''
     def train_step(self, src_input, dec_input, mcts_probs, actions,bleus):
         
-        if not self.encoder_output is None:
-            self.encoder_output = self.encoder_output.clone().detach()
+        self.encoder_output = None #don't want to use encoder output from previous iterations here
+        #if not self.encoder_output is None:
+        #    self.encoder_output = self.encoder_output.clone().detach()
         
         self.policy_optimizer.zero_grad()
         self.value_optimizer.zero_grad()
 
         #this will give first index where Blank
         sentence_lens = np.argmax((dec_input==globalsFile.BLANK_WORD_ID),0)
-
+        print('DECODER input: ',dec_input)
+        print('sentence_lens: ',sentence_lens)
+        
         src_input = src_input.to(self.device)
         dec_input = dec_input.to(self.device)
         mcts_probs = mcts_probs.to(self.device)
@@ -267,7 +274,10 @@ class PolicyValueNet():
 
         processes = torch.tensor(np.arange(src_input.shape[1])).long()
         # forward pass : args: src_tensor, dec_input, sentence_lens,req_grad
+        
         log_act_probs, value = self.forward(src_input, dec_input, processes=processes,sentence_lens=sentence_lens,req_grad=True)
+        
+        log_act_probs = torch.log(log_act_probs)
         #value is just array of value per element in batch
         #log_act_probs has shape (batch_size,vocab_size)
         

MultiProcess/runMultiProcessWithQueue.py
@@ -24,7 +24,7 @@ from mcts_translator import MCTS
 import time
 import json
 from dataBuffer import DataBuffer
-
+from lossAndTestClasses import *
 
 
 def run(rank,maxlen,main_params,trg_tensor,queue,queue_exit):
@@ -50,195 +50,212 @@ def run(rank,maxlen,main_params,trg_tensor,queue,queue_exit):
 
 #this is where the main process runs
 def main_func(numProcesses,src_tensor,maxlen,model,queue,queue_exit):
-	#exit('Exiting 1')
-	
-	model.policy_net = model.policy_net.to(model.device)
-	model.value_net = model.value_net.to(model.device)
-    
-    
-	starting_time = time.time()
-	
-	#RUN MODEL WITH ENTIRE SOURCE TENSOR ONCE TO GET 
-	#ENCODER OUTPUT:
-	#from then on we'll be using slices of it
-	dec_input = (torch.ones(numProcesses-1)*globalsFile.BOS_WORD_ID).long().view(1,-1)
-	processes = torch.tensor(np.arange(1,numProcesses)).long()
-	dec_lengths = torch.ones(numProcesses-1).long()
-	log_probs, values = model.forward(src_tensor,dec_input,processes=processes,
-										sentence_lens=dec_lengths,req_grad=False)
 
-	print('done setting encoder output from main')
+    starting_time = time.time()
 
-	exited_processes = []
-	while(True):
-		
-		#first receive tensors from subprocesses in queue
-		recv_tensors = []
-		start_time=time.time()
-		while(True):
-			if not queue.empty():
-				rec_tens = queue.get()
-				recv_tensors.append(rec_tens)
-			
-			if len(recv_tensors) > numProcesses-2 or time.time()-start_time>0.1:
-					break
+    #RUN MODEL WITH ENTIRE SOURCE TENSOR ONCE TO GET 
+    #ENCODER OUTPUT:
+    #from then on we'll be using slices of it
+    model.encoder_output = None #want new output corresponding to this source tensor
 
-		if len(recv_tensors) > 0:
-			#first get list of lengths and processes these came from
-			dec_lengths = torch.tensor([x[-2] for x in recv_tensors]).long()
-			processes = torch.tensor([x[-1].item() for x in recv_tensors]).long()
+    dec_input = (torch.ones(numProcesses-1)*globalsFile.BOS_WORD_ID).long().view(1,-1)
+    processes = torch.tensor(np.arange(1,numProcesses)).long()
+    dec_lengths = torch.ones(numProcesses-1).long()
+    log_probs, values = model.forward(src_tensor,dec_input,processes=processes,
+                                        sentence_lens=dec_lengths,req_grad=False)
 
-			#trim them down to the maximum length of all gathered when remove padding
-			max_gathered_len = int(dec_lengths.max().item())
-			assert(max_gathered_len > 0)
-			#print('RECEIVED TENSORS: ',recv_tensors[0])
-			dec_input = torch.cat([x[:max_gathered_len].view(-1,1) for x in recv_tensors],1).long()
-			#print('RECEIVED TENSORS: ',recv_tensors[0][:max_gathered_len])
-			src_slice = src_tensor[:,(processes-1)] #take processes-1 since subprocesses start at rank=1
-			#need to rearrange columns of src_tensor to work with 
-			#this input
-			#now encoder output, should do one initial run with all the sentences
-			#then use slices thereafter.  
+    print('done setting encoder output from main')
 
-			#mask for decoder_input happens within this function
-			log_probs, values = model.forward(src_slice,dec_input,processes=processes,
-											sentence_lens=dec_lengths,req_grad=False)
+    exited_processes = []
+    while(True):
 
-			#need to get top model.num_children probs and their corresponding actions
-			#which are the indices
-			#print('log probs shape: ',log_probs.shape)
-			sorted_probs,inds = torch.sort(log_probs,dim=1,descending=True)
-			inds = inds.double() #so that concat with probs and values
+        #first receive tensors from subprocesses in queue
+        recv_tensors = []
+        start_time=time.time()
+        while(True):
+            if not queue.empty():
+                rec_tens = queue.get()
+                recv_tensors.append(rec_tens)
 
-			#now stack sorted_probs under inds then put value underneath, (this is what 
-			#the other processes are expecting as format)
-			to_scatter = torch.cat([inds[:,:model.num_children].transpose(0,1),
-								sorted_probs[:,:model.num_children].transpose(0,1),
-								values.unsqueeze(0)], dim=0).to('cpu')
-			
-			to_scatter = list(np.split(to_scatter,to_scatter.shape[1],axis=1))
+            if len(recv_tensors) > numProcesses-2 or time.time()-start_time>0.1:
+                    break
 
-			#need to clone compoennets so that they don't share memory
-			to_scatter = [t.clone().squeeze(1) for t in to_scatter]
-			
-			#now send each of these to processes
-			for i,process in enumerate(processes):
-				dist.isend(tensor=to_scatter[i].clone(),dst=int(process))
+        if len(recv_tensors) > 0:
+            #first get list of lengths and processes these came from
+            dec_lengths = torch.tensor([x[-2] for x in recv_tensors]).long()
+            processes = torch.tensor([x[-1].item() for x in recv_tensors]).long()
+
+            #trim them down to the maximum length of all gathered when remove padding
+            max_gathered_len = int(dec_lengths.max().item())
+            assert(max_gathered_len > 0)
+            #print('RECEIVED TENSORS: ',recv_tensors[0])
+            dec_input = torch.cat([x[:max_gathered_len].view(-1,1) for x in recv_tensors],1).long()
+            #print('RECEIVED TENSORS: ',recv_tensors[0][:max_gathered_len])
+            src_slice = src_tensor[:,(processes-1)] #take processes-1 since subprocesses start at rank=1
+            #need to rearrange columns of src_tensor to work with 
+            #this input
+            #now encoder output, should do one initial run with all the sentences
+            #then use slices thereafter.  
+
+            #mask for decoder_input happens within this function
+            log_probs, values = model.forward(src_slice,dec_input,processes=processes,
+                                            sentence_lens=dec_lengths,req_grad=False)
+
+            #need to get top model.num_children probs and their corresponding actions
+            #which are the indices
+            #print('log probs shape: ',log_probs.shape)
+            sorted_probs,inds = torch.sort(log_probs,dim=1,descending=True)
+            inds = inds.double() #so that concat with probs and values
+
+            #now stack sorted_probs under inds then put value underneath, (this is what 
+            #the other processes are expecting as format)
+            to_scatter = torch.cat([inds[:,:model.num_children].transpose(0,1),
+                                sorted_probs[:,:model.num_children].transpose(0,1),
+                                values.unsqueeze(0)], dim=0).to('cpu')
+
+            to_scatter = list(np.split(to_scatter,to_scatter.shape[1],axis=1))
+
+            #need to clone compoennets so that they don't share memory
+            to_scatter = [t.clone().squeeze(1) for t in to_scatter]
+
+            #now send each of these to processes
+            for i,process in enumerate(processes):
+                dist.isend(tensor=to_scatter[i].clone(),dst=int(process))
 
 
-		#now check for processes having exited
-		if not queue_exit.empty():
-			exited_processes.append(queue_exit.get())
+        #now check for processes having exited
+        if not queue_exit.empty():
+            exited_processes.append(queue_exit.get())
 
-		#print(len(exited_processes))
-		if len(exited_processes) == numProcesses-1:
-			print('everyones exited')
-			print(time.time()-starting_time2)
-			exit()
+        #print(len(exited_processes))
+        if len(exited_processes) == numProcesses-1:
+            print('everyones exited')
+            #print(time.time()-starting_time2)
+            exit()
 
-	
+
 def init_processes(rank,numProcesses,src_tensor,trg_tensor,modelToPass,main_params,queue,queue_exit):
-	#using spawn_processes passes process index as first param
+    #using spawn_processes passes process index as first param
 
-	#create the processes here then have our workers call one function
-	#and our main process (rank=0) call another
-	os.environ['MASTER_ADDR'] = '127.0.0.1'
-	os.environ['MASTER_PORT'] = '29501'
-	print('rank: {}, numProcesses: {}'.format(rank,numProcesses))
-	dist.init_process_group('gloo', rank=rank, world_size=numProcesses)
-	#groupMCTS = dist.new_group([i for i in range(numProcesses)])
-	
-	maxlen = trg_tensor.shape[0] + 5 #max number of decode steps/ length in MCTS
-	if rank == 0:
-		main_func(numProcesses,src_tensor,maxlen,modelToPass,queue,queue_exit)
-	else:
-		run(rank,maxlen,main_params,trg_tensor[:,rank-1],queue,queue_exit)
+    #create the processes here then have our workers call one function
+    #and our main process (rank=0) call another
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '29502'
+    print('rank: {}, numProcesses: {}'.format(rank,numProcesses))
+    dist.init_process_group('gloo', rank=rank, world_size=numProcesses)
+    #groupMCTS = dist.new_group([i for i in range(numProcesses)])
+
+    maxlen = trg_tensor.shape[0] + 5 #max number of decode steps/ length in MCTS
+    if rank == 0:
+        main_func(numProcesses,src_tensor,maxlen,modelToPass,queue,queue_exit)
+    else:
+        run(rank,maxlen,main_params,trg_tensor[:,rank-1],queue,queue_exit)
 
 
 
 
 if __name__=='__main__':
-	
-	torch.multiprocessing.set_start_method('spawn')
-	#now create data_set iterators
-	dataset_dict = createIterators(globalsFile.BATCHSIZE,globalsFile.DATAPATH)
 
-	eng_vocab = dataset_dict['TGT'].vocab.itos
+    torch.multiprocessing.set_start_method('spawn')
+    #now create data_set iterators
+    dataset_dict = createIterators(globalsFile.BATCHSIZE,globalsFile.DATAPATH)
 
-	src_vocab_size = len(dataset_dict['SRC'].vocab.itos)
-	main_params = MainParams(dropout=0.2, src_vocab_size=src_vocab_size,
-                  tgt_vocab_size=len(eng_vocab), batch_size=5,l2_const=1e-4,
+    eng_vocab = dataset_dict['TGT'].vocab.itos
+
+    src_vocab_size = len(dataset_dict['SRC'].vocab.itos)
+    main_params = MainParams(dropout=0.2, src_vocab_size=src_vocab_size,
+                  tgt_vocab_size=len(eng_vocab), batch_size=5,l2_const=0,
                   c_puct=0.5,num_sims=1000,temperature=1e-3,
                   tgt_vocab_itos=dataset_dict['TGT'].vocab.itos,
-                  num_children=50,is_training=False)
+                  num_children=50,is_training=False,
+                  num_grad_steps_per_epoch=4,
+                  adamlr=1e-4)
 
 
-	policy_path = globalsFile.MODELPATH + 'policy_supervised_RLTrained.pt'
-	value_path = globalsFile.MODELPATH + 'value_supervised_RLTrained.pt'
-	#policy_path = None
-	#value_path = None #want to use cpu for now
-	network = PolicyValueNet(main_params=main_params,path_to_policy=policy_path,
-							path_to_value=value_path)
-	
-	network.policy_net = network.policy_net.to('cpu')
-	network.value_net = network.value_net.to('cpu')
-	print('TRANSFERRED TO CPU')
+    policy_path = globalsFile.MODELPATH + 'policy_supervised_RLTrained.pt'
+    value_path = globalsFile.MODELPATH + 'value_supervised_RLTrained.pt'
+    #policy_path = None
+    #value_path = None #want to use cpu for now
+    network = PolicyValueNet(main_params=main_params,path_to_policy=policy_path,
+                            path_to_value=value_path,adamlr=main_params.adamlr)
+
+    loss_history = LossHistory()
     
-	data_buffer = DataBuffer()
-	for epoch in range(1000): #number iterations over dataset
-		data_iter = 'train_iter'
-		sentences_processed = 0
-		for batch_count, batch in enumerate(dataset_dict[data_iter]):
-			#print('src shape: {}, tgt shape: {}'.format(vars(batch)['de'].shape,vars(batch)['en'].shape))
-			
-			src_tensor = vars(batch)['de'] #will be (S,batch_size) where S is max num tokens of sentence in this batch
-			trg_tensor = vars(batch)['en'] 
-			sentences_processed += src_tensor.shape[1]
-			
-			#REMOVE THIS
-			if src_tensor.shape[0] != 45:
-				continue
+    data_buffer = DataBuffer()
+    for epoch in range(1000): #number iterations over dataset
+        data_iter = 'train_iter'
+        sentences_processed = 0
+        for batch_count, batch in enumerate(dataset_dict[data_iter]):
+            #print('src shape: {}, tgt shape: {}'.format(vars(batch)['de'].shape,vars(batch)['en'].shape))
 
-			main_params.is_training = True if 'train' in data_iter else False
-			
-			#now create processes to use this
-			size = src_tensor.shape[1]+1 #one process per sentence and one to do computation
-			print('sentence len: ',src_tensor.shape[0])
-			starting_time = time.time()
-			
-			queue = Queue()
-			queue_exit = Queue()
-			ps = []
-			for rank in range(size):
-				model = network if rank==0 else None
-				args =(rank,size,src_tensor,trg_tensor,model,main_params,queue,queue_exit)
-				p = Process(target=init_processes,args=args)
-				p.start()
-				ps.append(p)
+            src_tensor = vars(batch)['de'] #will be (S,batch_size) where S is max num tokens of sentence in this batch
+            trg_tensor = vars(batch)['en'] 
+            sentences_processed += src_tensor.shape[1]
 
-			ps[0].join()
+            #REMOVE THIS
+            if src_tensor.shape[0] > 10:
+                continue
+
+            main_params.is_training = True if 'train' in data_iter else False
+
+            #now create processes to use this
+            size = src_tensor.shape[1]+1 #one process per sentence and one to do computation
+            print('sentence len: ',src_tensor.shape[0])
+            starting_time = time.time()
+
+            queue = Queue()
+            queue_exit = Queue()
+            ps = []
+            for rank in range(size):
+                model = network if rank==0 else None
+                args =(rank,size,src_tensor,trg_tensor,model,main_params,queue,queue_exit)
+                p = Process(target=init_processes,args=args)
+                p.start()
+                ps.append(p)
+
+            ps[0].join()
+
+            print('sentence len: ',src_tensor.shape[0])
+            print('totalTime: ',time.time()-starting_time)
+
+            print('Finished BATCH')
+
+            for rankNum in range(1,size):
+                fileName = globalsFile.CODEPATH+'/MCTSFiles/rank'+str(rankNum)+'.json'
+                with open(fileName) as f:
+                    data = json.load(f)
+                    #[bleu,output_states,mcts_probs,actions]
+                    src_ = src_tensor[:,rankNum-1]
+                    data_buffer.add_examples(src_,data)
+
+            #print('sent all to files')
+            #exit()
+
+            if sentences_processed % 500:
+                break
+        
+        
+        #now do some updates to the model
+        #HOW MANY TRAINING STEPS/SIZE TO DO? 
+        data_buffer.set_iterator(batch_size=main_params.batch_size,shuffle=True)
+        for grad_step in range(main_params.num_grad_steps_per_epoch):
+            src_,dec_,actions,probs,bleus = data_buffer.next()	
+            if src_ is None:
+                break
             
-			print('sentence len: ',src_tensor.shape[0])
-			print('totalTime: ',time.time()-starting_time)
-
-			print('Finished BATCH')
-
-			for rankNum in range(1,size):
-				fileName = globalsFile.CODEPATH+'/MCTSFiles/rank'+str(rankNum)+'.json'
-				with open(fileName) as f:
-					data = json.load(f)
-					#[bleu,output_states,mcts_probs,actions]
-					src_ = src_tensor[:,rankNum-1]
-					data_buffer.add_examples(src_,data)
-
-			#print('sent all to files')
-			#exit()
-
-			if sentences_processed % 500:
-				#now do some updates to the model
-				while(True):
-					data_buffer.set_iterator(batch_size=main_params.batch_size,shuffle=True)
-					src_,dec_,actions,probs,bleus = data_buffer.next()	
-					if src_.shape[1] < main_params.batch_size:
-						break
-					loss = network.train_step(src_,dec_,probs,actions,bleus)
+            loss = network.train_step(src_,dec_,probs,actions,bleus)
+            loss_history.add_batch_loss(loss,-1, 'train')
+        
+        #get validation loss
+        bleu = get_policy_ave_bleu(network.policy_net,dataset_dict,'val',network.device,num_decode_steps=-1,useBLEU1=False)
+        loss_history.add_batch_loss(bleu,-1, 'valid')
+        loss_history.add_epoch_loss(epoch)
+         
+            
+            
+            
+            
+            
+            
+            
+            